a97106e5e5e80c3f2b5c94baa86d7567d568b022
fum-is-chum <alvinardiansyah2002@gmail.com>
2024-01-08 02:06:39 +0700

    feat: remove unused script

--- deployments/docker-images/build-all.sh
+++
@@ -1,6 +0,0 @@
-cd . && \
-docker-compose build && \
-docker tag todo-list-backend:latest localhost:5000/todo-list-backend:latest && \
-docker tag todo-list-frontend:latest localhost:5000/todo-list-frontend:latest && \
-docker push localhost:5000/todo-list-backend:latest && \
-docker push localhost:5000/todo-list-frontend:latest
--- deployments/scripts/build-and-push-images.sh
+++ deployments/scripts/build-and-push-images.sh
@@ -1,3 +1,4 @@
+# We only need to start registry container, the rest of the containers will be built and pushed to the registry and started by kubernetes
 cd ./ && docker-compose down && docker-compose build \
 && docker-compose up -d registry \
 && docker tag todo-list-frontend:latest localhost:5000/todo-list-frontend:latest \
