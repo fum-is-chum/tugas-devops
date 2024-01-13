c7522e9bf917448239b75c8a42c588430d10f38d
fum-is-chum <alvinardiansyah2002@gmail.com>
2024-01-13 16:08:35 +0700

    feat: change registry port to 50000

--- deployments/scripts/build-and-push-images.sh
+++ deployments/scripts/build-and-push-images.sh
@@ -1,8 +1,9 @@
-# We only need to start registry container, the rest of the containers will be built and pushed to the registry and started by kubernetes
+# We only need to start registry container, the rest of the containers will be built and 
+# pushed to the registry and started by kubernetes
 cd ./ && docker-compose down && docker-compose build \
 && docker-compose up -d registry \
-&& docker tag todo-list-frontend:latest localhost:5000/todo-list-frontend:latest \
-&& docker push localhost:5000/todo-list-frontend:latest \
-&& docker tag todo-list-backend:latest localhost:5000/todo-list-backend:latest \
-&& docker push localhost:5000/todo-list-backend:latest \
+&& docker tag todo-list-frontend:latest localhost:50000/todo-list-frontend:latest \
+&& docker push localhost:50000/todo-list-frontend:latest \
+&& docker tag todo-list-backend:latest localhost:50000/todo-list-backend:latest \
+&& docker push localhost:50000/todo-list-backend:latest \
 && docker image prune -f
